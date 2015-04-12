9e9bc1acb7d0775ec68dc6f81ba01b9f235240d9
Adrian <aloha@adrianperez.info>
2015-04-12 19:17:08 +0200

    headers

--- friends/la_bete.js
+++ friends/la_bete.js
@@ -1,15 +1,12 @@
 var date = new Date();
+var art  = $('article');
 
 var init = function(){
-	var mon_array = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-		mon_array[date.getUTCMonth()];
-
-	get_10pics();
+	get_10pics(art);
 }
 
 var get_10pics = function() {
-	var _url = 'https://api.data.gov/nasa/planetary/apod?date=2015-04-10&concept_tags=True&api_key=qfZlUQz6uJMKuQxDAQiuSC5G4XZrMq7RaJpC8nH6';
-	console.log('https://jsonp.afeld.me/?url=' + encodeURIComponent(_url))
+	var _url = 'https://api.data.gov/nasa/planetary/apod?date=' + date.getUTCFullYear() + '-' + date.getUTCMonth() + '-' + (date.getUTCDate()) +'&concept_tags=True&api_key=qfZlUQz6uJMKuQxDAQiuSC5G4XZrMq7RaJpC8nH6';
 	$.ajax({
 	    type : "GET",
 	    url : 'https://jsonp.afeld.me/?url=' + encodeURIComponent(_url),
@@ -20,11 +17,20 @@ var get_10pics = function() {
     	contentType: "application/text; charset=utf-8",
 	    async : false,
 	    success : function(data){
-	        console.log(data);},
+	    	if (!!data.url)
+	        	print_picture(data);
+	        else
+	        	console.log('getting yesterday')//get_10pics();
+	    },
 	    error : function(httpReq,status,exception){
 	        console.log(status+" "+exception);
 	    }
 	});
+}
+
+var print_picture = function(data) {
+	art.find('img').attr('src',data.url);
+	art.find('time').attr('datetime', date).html(date.toLocaleString(0,{ weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }));
 }
 
 init();
